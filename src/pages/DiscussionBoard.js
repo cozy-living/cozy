a81ea3ab1e4c589c246e665d305114d55701c7b2
kevinliu0605 <aidisheng880@gmail.com>
2022-02-07 20:51:07 -0500

    add CreatePost with CreateForm

--- src/pages/DiscussionBoard.js
+++ src/pages/DiscussionBoard.js
@@ -1,9 +1,12 @@
+import CreatePost from "../components/DiscussBoard/CreatePost";
+
 const DiscussionBoard = () => {
-    return (
-      <div className='centered'>
-        <p>The DiscussionBoard page</p>
-      </div>
-    );
-  };
-  
-  export default DiscussionBoard;
+  return (
+    <div className="centered">
+      <CreatePost />
+      <p>The DiscussionBoard page</p>
+    </div>
+  );
+};
+
+export default DiscussionBoard;
