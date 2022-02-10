da9cf271a6d11469471f31d48af827bf642530a9
YONG LI <85102180+462702985@users.noreply.github.com>
2022-02-09 20:24:33 -0500

    Add files via upload

--- src/pages/ServiceBoard.js
+++ src/pages/ServiceBoard.js
@@ -8,7 +8,7 @@ class ServiceBoard extends Component {
 
   render() {
     return (
-      <div>
+      <div classname='centered'>
         <h1>Service</h1>
         {
         this.state.admin ? <AdminService />
