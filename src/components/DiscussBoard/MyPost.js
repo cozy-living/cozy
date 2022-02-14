f71a6e242f23d1bacab38be22a7727198349097d
kevinliu0605 <aidisheng880@gmail.com>
2022-02-14 02:56:34 -0500

    add comment into the post page

--- src/components/DiscussBoard/MyPost.js
+++ src/components/DiscussBoard/MyPost.js
@@ -6,32 +6,32 @@ import MyPostButton from "./MyPostButton";
 
 const MyPost = () => {
   return (
-    <div>
-      <Col span={20}>
+    <div className={classes.container}>
+      <Col span={16}>
         <Card className={classes.card} title="User1">
           My Post #1
           {<MyPostButton />}
         </Card>
       </Col>
-      <Col span={20}>
+      <Col span={16}>
         <Card className={classes.card} title="User2">
           My Post #2
           {<MyPostButton />}
         </Card>
       </Col>
-      <Col span={20}>
+      <Col span={16}>
         <Card className={classes.card} title="User3">
           My Post #3
           {<MyPostButton />}
         </Card>
       </Col>
-      <Col span={20}>
+      <Col span={16}>
         <Card className={classes.card} title="User3">
           My Post #4
           {<MyPostButton />}
         </Card>
       </Col>
-      <Col span={20}>
+      <Col span={16}>
         <Card className={classes.card} title="User3">
           My Post #5
           {<MyPostButton />}
--- src/components/DiscussBoard/Post.js
+++ src/components/DiscussBoard/Post.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Card, Col, Row } from "antd";
 import classes from "./Post.module.css";
 import CreateComment from "./CreateComment";
+import AddComment from "./AddComment";
 
 const Post = () => {
   const getText = () => {
@@ -10,37 +11,52 @@ const Post = () => {
   };
 
   return (
-    <div>
-      <Col span={20}>
+    <div className={classes.container}>
+      <Col span={16}>
         <Card className={classes.card} title="User1">
           Post1
           {<CreateComment />}
         </Card>
       </Col>
-      <Col span={20}>
+
+      <AddComment />
+
+      <Col span={16}>
         <Card className={classes.card} title="User2">
           Post2
           {<CreateComment />}
         </Card>
       </Col>
-      <Col span={20}>
+
+      <AddComment />
+
+      <Col span={16}>
         <Card className={classes.card} title="User3">
           Post3
           {<CreateComment />}
         </Card>
       </Col>
-      <Col span={20}>
+
+      <AddComment />
+
+      <Col span={16}>
         <Card className={classes.card} title="User3">
           Post4
           {<CreateComment />}
         </Card>
       </Col>
-      <Col span={20}>
+
+      <AddComment />
+
+      <Col span={16}>
         <Card className={classes.card} title="User3">
           Post5
           {<CreateComment />}
         </Card>
       </Col>
+
+      <AddComment />
+
     </div>
   );
 };
